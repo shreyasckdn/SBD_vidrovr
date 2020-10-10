38cf79bee20fef064e0333ed48a5c929a7ec2d75
Shreyas Chandrakaladharan <shreyas.chandrakaladharan@gmail.com>
2020-10-10 15:28:04 -0400

    including line on targets

--- data.py
+++ data.py
@@ -21,7 +21,12 @@ def get_random_data(n_batches, batch_size, T, N):
          - additional frames added to base sequence: N
     """
     random_data_sample = torch.rand((n_batches, batch_size, C, T + N, H, W)) ## Random samples to emulate video sequence
-    random_data_target = torch.stack((torch.zeros(n_batches,batch_size,1+N), torch.ones(n_batches,batch_size,1+N)), dim=2)
+    
+    ## Random targets is of shape n_batches*batch_size*2*(1+N). 
+    ## Each 2*1 tensor is [0,1]. It represents that the target is True i.e there is a shot boundary there. 
+    ## Currently, we assume all the frames have shot boundaries. So all targets are True
+
+    random_data_target = torch.stack((torch.zeros(n_batches,batch_size,1+N), torch.ones(n_batches,batch_size,1+N)), dim=2) 
 
     data = [(x,y) for x,y in zip(random_data_sample,random_data_target)]
     
